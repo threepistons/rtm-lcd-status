1e7cd35244ec78fb1c3fb91d70a6f70f25d5dd10
Helen Griffiths <threepistons@users.noreply.github.com>
2017-12-14 02:51:41 +0000

    add .ini format support and .conf file

--- rtmstatus.py
+++ rtmstatus.py
@@ -4,27 +4,21 @@ import sys
 import signal
 import webbrowser
 from rtmapi import Rtm
-from rtmstatusconf import Rtmsettings
+import ConfigParser
 import time
 from lcdbackpack import LcdBackpack
 
-class Taskcounter:
-    # This class gets an integer number of tasks out of RTM, according to the supplied filter.
-    # e.g.
-    #    moo = Taskcounter('status:incomplete')
-    #    print str(moo.count) # gives number of open tasks
-
-    def __init__(self, myfilter):
-        self.myfilter = myfilter
-        count = 0
-        result = api.rtm.tasks.getList(filter=self.myfilter)
-        for tasklist in result.tasks:
-            for taskseries in tasklist:
-                count += 1
-        self.count = count
+def taskcounter (myfilter):
+    count = 0
+    result = api.rtm.tasks.getList(filter=myfilter)
+    for tasklist in result.tasks:
+        for taskseries in tasklist:
+            count += 1
+    return(count)
         
-def setbacklight(status):
-    display.set_backlight_rgb(settings.colours[status][0],settings.colours[status][1],settings.colours[status][2])
+def setbacklight(colour):
+    rgb = colour.split(',')
+    display.set_backlight_rgb(int(rgb[0]),int(rgb[1]),int(rgb[2]))
         
 def quitter(signal, frame):
     print('Quitting...')
@@ -44,8 +38,9 @@ if __name__ == '__main__':
     debug = True if (len(sys.argv) > 1 and sys.argv[1] == 'debug') else False
     colourtest = True if (len(sys.argv) > 1 and sys.argv[1] == 'colourtest') else False
 
-    settings = Rtmsettings()
-    api = Rtm(settings.main['api_key'], settings.main['shared_secret'], 'read', settings.main['token'])
+    config = ConfigParser.SafeConfigParser()
+    config.readfp(open('rtmstatus.conf'))
+    api = Rtm(config.get('main', 'api_key'), config.get('main', 'shared_secret'), 'read', config.get('main', 'token'))
 
     # authentication block, see http://www.rememberthemilk.com/services/api/authentication.rtm
     # check for valid token
@@ -67,33 +62,21 @@ if __name__ == '__main__':
     print('Press Ctrl+C to quit')
     
     while True:
-        o = Taskcounter(settings.filters['overdue'])
-        oi = Taskcounter(settings.filters['overdue_important'])
-        s = Taskcounter(settings.filters['soon'])
-        si = Taskcounter(settings.filters['soon_important'])
-        
+        count = {}
+        sections = config.sections()
+        sections.sort(reverse=True)
+        for section in sections:
+            if section != 'main':
+                count[section] = taskcounter(config.get(section, 'filter'))
         display.clear()
+        setbacklight(config.get('main', 'defaultcolour'))
+        for section in sections:
+            if section != 'main':        
+                display.set_cursor_position(int(config.get(section,'x')),int(config.get(section,'y')))
+                display.write(config.get(section,'label') + ': ' + str(count[section]))
+                if (int(count[section]) > int(config.get(section,'threshold'))):
+                    setbacklight(config.get(section, 'colour'))
+ 
 
-        display.write("O'due: " + str(o.count) + ' Imp: ' + str(oi.count))
-        display.set_cursor_position(1,2)
-        display.write("Soon: " + str(s.count) + ' Imp: ' + str(si.count))
-
-        if (oi.count > 0):
-            setbacklight('overdue_important')
-        elif (si.count > 0):
-            setbacklight('soon_important')
-        elif (o.count > 0):
-            setbacklight('overdue')
-        elif (s.count > 0):
-            setbacklight('soon')
-        else:
-            setbacklight('no_tasks')
-
-        if debug == True:
-           print settings.filters['overdue'] + ': ' + str(o.count)
-           print settings.filters['overdue_important'] + ': ' + str(oi.count)
-           print settings.filters['soon'] + ': ' + str(s.count)
-           print settings.filters['soon_important'] + ': ' + str(si.count)
-
-        time.sleep(settings.main['polling_delay'])
+        time.sleep(float(config.get('main','polling_delay')))
 
